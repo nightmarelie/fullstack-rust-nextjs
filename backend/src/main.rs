28bde565af4765cb15370887fa5f92c042f7db54
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-01-28 09:50:10 -0600

    feat(rust): add dependency

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,12 @@
+use postgres::{Client, NoTls};
+use postgres::Error;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
 fn main() {
     println!("Hello, world!");
 }
