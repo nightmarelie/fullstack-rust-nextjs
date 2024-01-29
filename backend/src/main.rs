9ea84f6121965af49fe811885bab59d0759ef6d5
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-01-29 09:50:10 -0600

    feat(rust): define constants

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -16,6 +16,14 @@ struct User {
     password: Option<String>,
 }
 
+// CONSTANTS
+const DATABASE_URL: &str = env!("DATABASE_URL");
+// Define some responses
+const OK_RESPONSE: &str =
+    "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
+
 fn main() {
     println!("Hello, world!");
 }
